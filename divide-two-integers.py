befd2f0a38dde4ec82ee54473e61402432098e1a
HEMANTH KUMAR  EK <125449369+hemanthek73@users.noreply.github.com>
2024-03-12 19:36:30 +0530

    Add files via upload

---
+++ divide-two-integers.py
@@ -0,0 +1,19 @@
+class Solution:
+    def divide(self, dividend: int, divisor: int) -> int:
+        d=abs(dividend)
+        dv=abs(divisor)
+        output=0
+        while d>=dv:
+            temp=dv
+            mul=1
+            while d>=temp:
+                d-=temp
+                output+=mul
+                mul+=mul
+                temp+=temp
+        if(dividend<0 and divisor>=0) or(divisor<0 and dividend>=0):
+            output=-output
+        return min(2147483647,max(-2147483648,output))
+       
+
+        
