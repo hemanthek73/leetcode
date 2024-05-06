909d601c04eb79f806e9002e38e9acc1e83efff1
HEMANTH KUMAR  EK <125449369+hemanthek73@users.noreply.github.com>
2024-05-06 20:47:34 +0530

    Add files via upload

---
+++ remove-nodes-from-linked-list.py
@@ -0,0 +1,26 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def removeNodes(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        def reverse(head):
+            prev,cur=None,head
+            while cur:
+                tmp=cur.next
+                cur.next=prev
+                prev,cur=cur,tmp
+            return prev
+        
+        head=reverse(head)
+        cur=head
+        cur_max=cur.val
+        while cur.next:
+            if cur.next.val<cur_max:
+                cur.next=cur.next.next
+            else:
+                cur_max=cur.next.val
+                cur=cur.next
+        return reverse(head)
+        
