7c270f977520b95fee3e8fe72d5c2bd729d0306d
Hemantha Kumara E K <125449369+hemanthek73@users.noreply.github.com>
2024-09-06 07:49:48 +0530

    Add files via upload

---
+++ dlete_node_from_linkedList.py
@@ -0,0 +1,20 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def modifiedList(self, nums: List[int], head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head:
+            return None
+        num_set=set(nums)
+        dummy=ListNode(0)
+        dummy.next=head
+        prev,curr=dummy,head
+        while curr:
+            if curr.val in num_set:
+                prev.next=curr.next
+            else:
+                prev=curr
+            curr=curr.next
+        return dummy.next
