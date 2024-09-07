dd466c983493de4e1a06f98122ae925153391541
Hemantha Kumara E K <125449369+hemanthek73@users.noreply.github.com>
2024-09-07 18:20:38 +0530

    Add files via upload

---
+++ 1367_Linked_list_binary_tree.py
@@ -0,0 +1,30 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSubPath(self, head: Optional[ListNode], root: Optional[TreeNode]) -> bool:
+        if self.helper(head,root):
+            return True
+        if not root:
+            return False
+        return(
+            self.isSubPath(head,root.left) or 
+            self.isSubPath(head,root.right)
+        )
+    def helper(self,list_node,tree_node):
+        if not list_node:
+            return True
+        if not tree_node or list_node.val!=tree_node.val:
+            return False
+        return(
+            self.helper(list_node.next,tree_node.left)or 
+            self.helper(list_node.next,tree_node.right)
+        )
