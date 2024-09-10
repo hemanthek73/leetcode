715313f4d0b5b879734865102ff1900f9d83f014
Hemantha Kumara E K <125449369+hemanthek73@users.noreply.github.com>
2024-09-10 06:40:40 +0530

    Add files via upload

---
+++ 2807_insert_gcd_linkedlist.py
@@ -0,0 +1,17 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def insertGreatestCommonDivisors(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev=head
+        cur=head.next
+        while cur:
+            gcd=math.gcd(cur.val,prev.val)
+            g=ListNode(gcd)
+            prev.next=g
+            g.next=cur
+            prev=cur
+            cur=cur.next
+        return head
